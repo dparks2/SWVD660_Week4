2ae40417bb2ede827fba4a04e4ac344509f31309
Maryville-SWDV-610-1W-18-SU2 <33027357+dparks2@users.noreply.github.com>
2019-02-05 10:18:43 -0600

    Add files via upload

---
+++ client.py
@@ -0,0 +1,21 @@
+import socket                
+
+client = socket.socket()
+ip = socket.gethostbyname(socket.gethostname())
+port = 9500 # default port for socket
+address = (ip,port)
+
+client.connect(address)
+
+
+def communicate(data):
+    client.send(data)
+    print (client.recv(1024))
+    return
+
+#Satisfies the requirement when the client "Hello". Should return a hi
+communicate(b'Hello')
+
+
+#Satisfies the requirement when the client any other word. Should return a goodbye
+communicate(b'Otherwords')
